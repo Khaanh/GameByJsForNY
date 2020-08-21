39194b0b3710440fdcf454e8bafd896a35749a78
Khanh <Khanh13Nguyen13@gmail.com>
2020-08-22 02:01:25 +0300

    Random coordinates function

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-let docWidth = window.innerWidth,
-    docHeight = window.innerHeight;
+let getWidth      = document.body.clientWidth,
+    getHeight     = window.innerHeight,
+    windowWidth   = document.getElementById('js-width'),
+    windowHeight  = document.getElementById('js-height'),
+    element       = document.getElementById('box');
 
-let winWidth = document.getElementById('js-width'),
-    winHeight = document.getElementById('js-height');
+windowWidth.textContent = `Width: ${getWidth}`;
+windowHeight.textContent = `Heigth: ${getHeight}`;
 
-winWidth.textContent = `Width: ${docWidth}`;
-winHeight.textContent = `Heigth: ${docHeight}`;
+// Получение случайного целого числа в заданном интервале для значении Y
+function getRandomCoordinatesY(minY ,maxY, minX, maxX) {
+  minY = Math.ceil(minY);
+  maxY = Math.floor(maxY);
+  minX = Math.ceil(minX);
+  maxX = Math.floor(maxX);
+
+  let randomY = Math.floor(Math.random() * (maxY - minY)) + minY;
+  let randomX = Math.floor(Math.random() * (maxX - minX)) + minX;
+
+  element.setAttribute('style', `top: ${randomY}px; left: ${randomX}px`)
+}
+getRandomCoordinatesY(0, getHeight, getWidth, 0)
+
+
+// function getRandomCoordinatesX(min ,max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+
+//   let randomX = Math.floor(Math.random() * (max - min)) + min;
+//   element.setAttribute('style', `left: ${randomX}px;`)
+// }
+// getRandomCoordinatesX(getWidth, 0)
+
