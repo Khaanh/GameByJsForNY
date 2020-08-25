ac12108b790dc0303efebdf3a4cde647cb61b1d4
Khanh <Khanh13Nguyen13@gmail.com>
2020-08-25 23:09:01 +0300

    Step by step

--- index.js
+++ index.js
@@ -8,29 +8,54 @@ let getWidth      = document.body.clientWidth,
 getHeight     = window.innerHeight,
 windowWidth   = document.getElementById('js-width'),
 windowHeight  = document.getElementById('js-height'),
-element       = document.getElementById('element'),
 elements      = document.querySelectorAll('.box');
 
 windowWidth.textContent = `Width: ${getWidth}`;
 windowHeight.textContent = `Heigth: ${getHeight}`;
 
 // Получение случайного целого числа в заданном интервале для значении X,Y
-function getRandomCoordinates(minY ,maxY, minX, maxX) {
-  minY = Math.ceil(minY);
-  maxY = Math.floor(maxY);
-  minX = Math.ceil(minX);
-  maxX = Math.floor(maxX);
-  
-  const randomY = Math.floor(Math.random() * (maxY - minY)) + minY;
-  const randomX = Math.floor(Math.random() * (maxX - minX)) + minX;
-  
-  // elements.setAttribute('style', `top: ${randomY}px; left: ${randomX}px`)
-  element.style.cssText = `top: ${randomY}px; left: ${randomX}px`;
-  
-}
-getRandomCoordinates(0, getHeight, 0, getWidth)
+// getRandomCoordinates(0, getHeight, 0, getWidth)
+  // function getRandomCoordinates(minY, maxY, minX, maxX, elements) {
+  //   minY = Math.ceil(minY);
+  //   maxY = Math.floor(maxY);
+  //   minX = Math.ceil(minX);
+  //   maxX = Math.floor(maxX);
+    
+  //   const randomY = Math.floor(Math.random() * (maxY - minY)) + minY;
+  //   const randomX = Math.floor(Math.random() * (maxX - minX)) + minX;
+    
+  //   // elements.setAttribute('style', `top: ${randomY}px; left: ${randomX}px`)
+  //   // element.style.cssText = `top: ${randomY}px; left: ${randomX}px`;
+    
+  //   console.log(`randomY: ${randomY}`);
+  //   console.log(`randomX: ${randomX}`);
 
-
-elements.forEach(function(item, index) {
-  console.log(`item: ${item} --- index: ${index}`);
-})
+  //   // for (let i = 0; i < elements.length; i++) {
+  //   //   console.log(elements[i]);
+  //   //   elements[i].style.cssText = `top: ${randomY}px; left: ${randomX}px`;
+      
+  //   // }
+  // }
+  
+  let i = 0;
+  
+  while (i < elements.length) {
+    function getRandomCoordinates(minY, maxY, minX, maxX, elements) {
+      minY = Math.ceil(minY);
+      maxY = Math.floor(maxY);
+      minX = Math.ceil(minX);
+      maxX = Math.floor(maxX);
+      
+      const randomY = Math.floor(Math.random() * (maxY - minY)) + minY;
+      const randomX = Math.floor(Math.random() * (maxX - minX)) + minX;
+      
+      console.log(`randomY: ${randomY}`);
+      console.log(`randomX: ${randomX}`);
+      return randomX;
+    }
+    
+    getRandomCoordinates(0, getHeight, 0, getWidth, elements);
+    // elements[i].style.cssText = `left: ${randomX}px`
+    console.log(elements[i]);
+    i++;
+  }
